4d69a05e5b2222cc9cc7f01f6229d342c5bf9109
Luis Fernando Sejas <nativuks@gmail.com>
2017-10-20 23:18:47 -0400

    Adding Material module,this module import de common components for angular material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MATERIAL_COMPATIBILITY_MODE,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatToolbarModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatDialogModule,
+  MatFormFieldModule 
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatListModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatInputModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    { provide: MATERIAL_COMPATIBILITY_MODE, useValue: true }
+  ],
+  declarations: []
+})
+export class MaterialModule { }
